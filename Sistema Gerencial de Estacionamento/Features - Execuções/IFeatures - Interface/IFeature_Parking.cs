095be6fcc419009d391516d2cd96d315360fadef
Paulo Arthur Araújo de Andrade <144744479+parthur207@users.noreply.github.com>
2024-10-17 15:21:05 -0300

    OK

--- Sistema Gerencial de Estacionamento/Features - Execuções/IFeatures - Interface/IFeature_Parking.cs
+++ Sistema Gerencial de Estacionamento/Features - Execuções/IFeatures - Interface/IFeature_Parking.cs
@@ -5,19 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sistema_Gerencial_de_Estacionamento.IFeatures
+namespace Sistema_Gerencial_de_Estacionamento.Features___Execuções.IFeatures___Interface
 {
     internal interface IFeature_Parking
     {
-        TimeSpan CheckOut(DateTime Entrada, DateTime Saida) 
+        TimeSpan CheckOut_periodo(DateTime Entrada, DateTime Saida) 
         {
             return TimeSpan.Zero;
         }
         void queryVenancies() { }
         void Exibition_Venancies(Tipo_Veiculo veiculo) { }
-
         string C_Radom() { return string.Empty; }
-
-        double Pagamento(TimeSpan Periodo) { return 0; }
+        double Calculo_Pagamento(TimeSpan Periodo) { return 0; }
+        void Delete_R() { }
     }
 }
